3fc1b2f3a618e68221e560c6163d511af14f151a
Kyle <1228073484@qq.com>
2017-08-11 22:31:11 +0800

    微信公众号 开发模式下的图文消息回复

--- src/main/java/com/weixin/util/WeiXinUtil.java
+++ src/main/java/com/weixin/util/WeiXinUtil.java
@@ -13,4 +13,5 @@ package com.weixin.util;
 public class WeiXinUtil {
     private static final String APPID = "wx0b68ae8c87cda01c";
     //AppSecret = 28277424282c90a5ac20d10e0046f1eb;//瑞和轩
+    private static final String APPSECRET = "28277424282c90a5ac20d10e0046f1eb";
 }
